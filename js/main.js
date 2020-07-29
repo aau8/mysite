d8de52b201879ec0078385878ff5f94b2b7e881a
artemyg0811 <artiom.ygryumov@gmail.com>
2020-07-29 19:04:31 +0300

    сделал первый блок с аккордеонами и контентом

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('.acc__link').on('click', function(){
+    $('.acc__info').not($(this).next()).slideUp(500);
+    $(this).next().slideToggle(500);
+  });
+  $('.link__phone').click(function(){
+    $('.phone__bg-up, .telegram__bg-down, .acc__img-phone, .acc__arrow-phone').toggleClass('active');
+    $('.designers__bg-up, .acc__img-designers, .acc__arrow-designers, .playlist__bg-up, .acc__img-playlist, .acc__arrow-playlist').removeClass('active');
+  })
+  $('.link__designers').click(function(){
+    $('.designers__bg-up, .acc__img-designers, .acc__arrow-designers').toggleClass('active');
+    $('.phone__bg-up, .telegram__bg-down, .acc__img-phone, .acc__arrow-phone, .playlist__bg-up, .acc__img-playlist, .acc__arrow-playlist').removeClass('active');
+  })
+  $('.link__playlist').click(function(){
+    $('.playlist__bg-up, .acc__img-playlist, .acc__arrow-playlist').toggleClass('active');
+    $('.phone__bg-up, .telegram__bg-down, .acc__img-phone, .acc__arrow-phone, .designers__bg-up, .acc__img-designers, .acc__arrow-designers').removeClass('active');
+  })
+});
